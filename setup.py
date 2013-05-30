a1a4fd47ea70437f65bd0b1c0e0f3c94c2abc6bc
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-30 11:18:55 +0530

    Add setup for buildout

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-help-pagess',
+    version='0.1.1',
+    description='forked the form steve app',
+    author='Steve',
+    author_email='steve@gmail.com',
+    url='https://github.com/shaijudavis/django-help-pages.git',
+    license='MIT',
+    packages=find_packages(),
+)
+
